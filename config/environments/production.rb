188b8993a7bd79a1b06130ec1069c1f3c1d35d44
shimeji221418 <shimeji2214@gmail.com>
2023-05-12 22:36:04 +0900

    create secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.require_master_key = true
 end
