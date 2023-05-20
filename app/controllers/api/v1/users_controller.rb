e38e874adad363501e741ea3e4b1b9be4f8604d5
shimeji221418 <shimeji2214@gmail.com>
2023-05-20 22:46:05 +0900

    firebase_id_token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -27,7 +27,6 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     end
 
     def current
-        pp User.all 
         currentUser = User.find_by(uid: params[:uid])
         if currentUser
             render status: 200 , json:  currentUser
