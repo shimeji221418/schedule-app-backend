951089883b25e99bac3c41bcbfc58e459e2447c3
shimeji221418 <shimeji2214@gmail.com>
2023-05-20 23:48:28 +0900

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :team
+  belongs_to :team, optional: true
   has_many :schedules, dependent: :destroy
   enum role: {general: 0, admin: 1}
   validates :name, uniqueness: true, length: { maximum: 20 }
